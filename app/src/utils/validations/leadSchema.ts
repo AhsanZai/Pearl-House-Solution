05cbc9a346babd9b2b4b1dbd0393e61bb7c94302
moiz-260 <moiz.khan@mergestack.com>
2026-03-28 10:02:37 +0500

    update form validations

--- app/src/utils/validations/leadSchema.ts
+++ app/src/utils/validations/leadSchema.ts
@@ -11,9 +11,5 @@ export const leadFormSchema = yup.object().shape({
   .matches(
     /^\+1[\d\s.-]+$/,
     "Phone number must be valid"
-  ),
-  zip: yup
-    .string()
-    .required("Zip code is required")
-    .matches(/^[0-9]{5}(?:-[0-9]{4})?$/, "Must be a valid ZIP code"),
+  )
 });
